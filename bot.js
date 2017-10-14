5c2cef1afd6bad0955ba8627b097b10867bde273
AaronKWalker <aaron.k.walker1@gmail.com>
2017-10-14 15:49:36 -0500

    testing log-symbols package

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 var chalk = require('chalk');
+var ls = require('log-symbols');
 var speedTest = require('speedtest-net');
 var test = speedTest({maxTime: 5000});
 
@@ -18,3 +19,12 @@ function whine(){
     throw error;
   })
 }
+
+function testConsole(){
+  console.log(ls.success, chalk.green('Success!'));
+  console.log(ls.info, chalk.blue('Info'));
+  console.log(ls.warning, chalk.yellow('Warning!'));
+  console.log(ls.error, chalk.red('Error!'));
+}
+
+testConsole();
