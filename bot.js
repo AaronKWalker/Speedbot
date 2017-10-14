b1513cff903bc08fbc3eb4e28417510b56f3201c
AaronKWalker <aaron.k.walker1@gmail.com>
2017-10-14 16:01:58 -0500

    testing speedtest-net npm package

--- bot.js
+++ bot.js
@@ -7,6 +7,21 @@ var test = speedTest({maxTime: 5000});
 
 var client = new Twitter(config);
 
+
+function testConsole(){
+  console.log(ls.success, chalk.green('Success!'));
+  console.log(ls.info, chalk.blue('Info'));
+  console.log(ls.warning, chalk.yellow('Warning!'));
+  console.log(ls.error, chalk.red('Error!'));
+}
+
+function testSpeed(){
+  speedTest.visual({maxTime: 10000}, (err, data)=> {
+    console.dir(`${data.speeds.download} Mbps`);
+    console.dir(`${data.speeds.upload} Mbps`);
+  })
+}
+
 function whine(){
   client.post('statuses/update', {status: 'testing no.6'})
   .then(function (tweet) {
@@ -20,11 +35,4 @@ function whine(){
   })
 }
 
-function testConsole(){
-  console.log(ls.success, chalk.green('Success!'));
-  console.log(ls.info, chalk.blue('Info'));
-  console.log(ls.warning, chalk.yellow('Warning!'));
-  console.log(ls.error, chalk.red('Error!'));
-}
-
-testConsole();
+testSpeed();
